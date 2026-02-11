929e1563ee0ece866031970d60b161563581d0e2
zafork <aroni_ar@hotmail.com>
2026-02-11 02:52:43 -0500

    feat: Agregando config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
